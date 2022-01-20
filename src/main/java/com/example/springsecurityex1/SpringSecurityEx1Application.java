774a22d5d338d9904d2d973a3fef64ae6c0a4ddc
Ilya <ilya.skavysh@gmail.com>
2022-01-20 15:08:49 +0300

    first commit

---
+++ src/main/java/com/example/springsecurityex1/SpringSecurityEx1Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityEx1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityEx1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecurityex1/SpringSecurityEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityEx1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
